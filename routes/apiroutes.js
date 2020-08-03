eac21607ae845d2554de8465ca27bb33f6618b2f
eradicador <janisepinto@yahoo.com>
2020-08-03 18:29:18 -0400

    added  3 more api routes

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -12,18 +12,40 @@ router.get("/api/workouts", (req, res) => {
 });
 
 
-// router.get("/api/workouts/range",
+router.get("/api/workouts/range", (req, res) => {
+    Workout.find().limit(7)
+        .then(dbWorkouts => {
+            res.json(dbWorkouts);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+})
 
-// )
 
-// router.post("/api/workouts", (req, res)
+router.post("/api/workouts", (req, res) => {
+    Workout.create({})
+        .then(dbWorkouts => {
+            res.json(dbWorkouts);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
 
-// )
 
 
-// router.put("/api/workouts/:id",
+router.put("/api/workouts/:id", (req, res) => {
+    Workout.findByIdAndUpdate(req.params.id,
+        { "$push": { "exercises": req.body } })
+        .then(dbWorkouts => {
+            res.json(dbWorkouts)
+        })
+        .catch(err => {
+            res.json(err);
+        })
 
-// )
+})
 
 
 module.exports = router;
